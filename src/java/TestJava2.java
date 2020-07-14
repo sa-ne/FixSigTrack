c3f90efb25a34d673f08aee8b4563569ebc1ecad
Amritpal Jhajj <amritpaljhajj@hotmail.com>
2020-07-13 20:33:53 -0400

    Add files via upload

---
+++ src/java/TestJava2.java
@@ -0,0 +1,9 @@
+public class TestJava2
+  {
+    private static String RHSA_CVE_CODE="RHSA-2017:2423_CVE-2019-17571";
+
+    public static void main(String[] args) 
+      {
+        System.out.println("This will be printed\n");
+      }
+  }
