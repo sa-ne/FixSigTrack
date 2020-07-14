e6a9698d02b4a01e6538594904155cff6638eb3f
Amritpal Jhajj <amritpaljhajj@hotmail.com>
2020-07-13 20:30:07 -0400

    Add files via upload

---
+++ src/c++/TestCPP2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+std::string RHSA_CVE_CODE = "RHSA-2017:2423_CVE-2019-17571";
+
+int main() 
+  {
+    std::cout << "Hello World!\n\n";
+    return(0);
+  }
